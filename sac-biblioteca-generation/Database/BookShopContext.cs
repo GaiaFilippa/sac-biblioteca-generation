feb0a267e6b64fc9d6ab05e70ecc4198c6e1119c
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-06-01 15:25:18 +0200

    created Bookshop database

---
+++ sac-biblioteca-generation/Database/BookShopContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sac_biblioteca_generation.Models;
+
+namespace sac_biblioteca_generation.Database
+{
+    public class BookShopContext : DbContext
+    {       
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookGenre> Genre { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=BookShop;" +
+                "Integrated Security=True;TrustServerCertificate=True");
+        }
+
+    }
+}
---
+++ sac-biblioteca-generation/Models/Book.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace sac_biblioteca_generation.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(150, ErrorMessage = "Il campo nome può contenere al massimo 100 caratteri")]
+        public string Titolo { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        public string Descrizione { get; set; }
+
+        public DateTime Prestito { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Url(ErrorMessage = "L'url inserito non è valido")]
+        public string UrlImmagine { get; set; }
+
+
+        public int GenreId { get; set; }
+        public BookGenre Genre { get; set; }
+
+        public Book()
+        {
+
+        }
+
+        public Book(string titolo, string descrizione, string urlImmagine)
+        {
+            Titolo = titolo;
+            Descrizione = descrizione;
+            UrlImmagine = urlImmagine;
+        }
+    }
+}
